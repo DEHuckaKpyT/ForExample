8aab35479baf449c7cad0c3c1e5e4661afd8ae67
DEHucka <d.matycin@white-soft.ru>
2021-06-07 13:15:24 +1000

    Initial commit

---
+++ src/main/java/hah/DenKpyT/ForExample/Main.java
@@ -0,0 +1,19 @@
+package hah.DenKpyT.ForExample;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        String string = in.nextLine();
+
+        int sum = 0;
+        int max = 0;
+        
+        for (int i = 0; i < string.length(); i++){
+
+        }
+    }
+}
