534a78509b84faf1ef9b0f7f841830482f3a98a4
DEHucka <d.matycin@white-soft.ru>
2021-06-07 13:35:57 +1000

    .

--- src/main/java/hah/DenKpyT/ForExample/Main.java
+++ src/main/java/hah/DenKpyT/ForExample/Main.java
@@ -11,9 +11,18 @@ public class Main {
 
         int sum = 0;
         int max = 0;
-        
+        int min = 9;
         for (int i = 0; i < string.length(); i++){
-
+            int digit = string.charAt(i) - '0';
+            sum += digit;
+            if (digit > max) max = digit;
+            if (digit < min) min = digit;
         }
+
+        System.out.println("Sum = " + sum);
+        System.out.println("Max = " + max);
+        System.out.println("Average = " + sum/string.length());
+        System.out.println("Min = " + min);
+
     }
 }
